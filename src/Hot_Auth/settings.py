729aa61972e57b279520f21dea597cdb8027b475
Kaitlyn Friden <kfriden@verizon.net>
2021-11-15 21:19:46 -0500

    updates

--- src/Hot_Auth/settings.py
+++ src/Hot_Auth/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import dj_database_url
+import django_heroku
 from pathlib import Path
 import os
-# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -185,3 +185,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
